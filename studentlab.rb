f666989cb034e82a2a07ca8c72712ef69dbe6d1d
earwerm <michael@earwerm.com>
2013-06-19 17:42:20 +1000

    Add effort 5:42pm

--- studentlab.rb
+++ studentlab.rb
@@ -8,10 +8,13 @@ j = gets.chomp.to_i
 lab = []
 new_group = []
 
-
 while members.length > 0
   new_group = members.pop j
-  lab.push new_group
-  if new_group.length <= j
-  end
+if new_group.length == j
+  lab = lab.push new_group
+else
+  new_group.push lab
+ end
 end
+
+puts "The resulting lab groups are #{lab}"
