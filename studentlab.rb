604048aaae6b247e92a5a688d32229cd892c80db
earwerm <michael@earwerm.com>
2013-06-20 10:25:58 +1000

    post presentation final

--- studentlab.rb
+++ studentlab.rb
@@ -1,16 +1,16 @@
 students = "Dan Willy Jack Hui Andrew Stephen Sanjita Tarun Nicholas Mike Ryan".split.shuffle
 
-puts "These are your class members #{students},"
+puts "These are your class students #{students},"
 puts "There are a total of #{students.length} of them."
-puts "Please select a number between 1 and #{students.length/2} to break them into random lab groups."
+puts "Please select a number, between 1 and #{students.length/2}, to break them into random lab groups."
 
-j = gets.chomp.to_i
+group_size = gets.chomp.to_i
 lab = []
 new_group = []
 
 while students.length > 0
-  new_group = students.pop j
-if new_group.length == j
+  new_group = students.pop group_size
+if new_group.length == group_size
   lab = lab.push new_group
 else
   lab = lab.push new_group
@@ -18,6 +18,5 @@ else
   lab.delete_at(-2)
  end
 end
-
 
 puts "The resulting lab groups are #{lab}"
